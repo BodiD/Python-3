d49cade1cd91a3dabde96723f3235d55d4d72aa5
BodiD <36726551+BodiD@users.noreply.github.com>
2019-09-03 14:00:25 +0200

    Create DIC.py
    8=>

---
+++ DIC.py
@@ -0,0 +1,16 @@
+"""
+dict = {'Name': 'Bodi', 'Age': 14, 'Class': 'First'}
+dict["Google"] = "Larry Page en Sergey Brin"
+print ("['Name']: ", dict['Name'])
+print ("['Age']: ", dict['Age'])
+print ("['Google]: ", dict['Google'])
+if "Google" in dict:
+    print("Google zit erin!")
+del dict["Age"]
+if "Age" not in dict:
+    print("TurboBUILD IS BACKKKCKCKCKKCKCKC!")
+"""
+    
+dic1={'1':'10', '2':'20'}
+dic2={'3':'30', '4':'40'}
+dic3={'5':'50', '6':'60'}
