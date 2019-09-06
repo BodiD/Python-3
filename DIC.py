1d3ccfdb6f10119302c18be33b0728ac18913b01
BodiD <36726551+BodiD@users.noreply.github.com>
2019-09-06 11:41:11 +0200

    Update DIC.py
    s

--- DIC.py
+++ DIC.py
@@ -10,14 +10,47 @@ del dict["Age"]
 if "Age" not in dict:
     print("TurboBUILD IS BACKKKCKCKCKKCKCKC!")
 """
-    
+   
+
+"""
+
 dic1={'1':'10', '2':'20'}
 dic2={'3':'30', '4':'40'}
 dic3={'5':'50', '6':'60'}
 def Merge(dic1, dic2):
-    return(dic1.update(dic2))
-def Merge2(dic1, dic3):
-    return(dic1.update(dic3))
+    return(dic2.update(dic1))
+def Merge2(dic2, dic3):
+    return(dic2.update(dic3))
 print(Merge(dic1, dic2))
-print(Merge2(dic1, dic3))
-print(dic1)
+print(Merge2(dic2, dic3))
+print(dic2)
+
+"""
+
+"""
+
+def groet():
+    for i in range(10):
+        print("hallo")
+        
+groet()
+
+
+def tafel5():
+    print("5, 10, 15, 20, 25, 30, 35, 40, 45, 50")
+    
+tafel5()
+ 
+ """
+
+def groet(naam):
+    print("Hallo "+ naam)
+    
+groet("Bodi")
+groet("Joël")
+groet("poep")
+ 
+ 
+ 
+ 
+ 
